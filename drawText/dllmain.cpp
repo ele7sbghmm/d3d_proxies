57f16b8dbbbe31f72d71f516762c62095e73de50
monte <poop>
2026-01-24 22:04:31 +1300

    proxy d3d8.dll

--- drawText/dllmain.cpp
+++ drawText/dllmain.cpp
@@ -1,77 +1,55 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
-
 #include <windows.h>
-#include <process.h>
-#include <string>
 #include <d3d9.h>
-#include <d3dx9.h>
 
+// 1. FORWARD BINK CALLS
+// These tell the game: "If you want to play a video, ask binkw32_original.dll"
 #pragma comment(lib, "d3d9.lib")
-#pragma comment(lib, "d3dx9d.lib")
-// #pragma comment(linker, "/export:Direct3DCreate9=C:\\Windows\\SysWOW64\\d3d9.Direct3DCreate9")
-#pragma comment(linker, "/export:Direct3DCreate9Ex=C:\\Windows\\SysWOW64\\d3d9.Direct3DCreate9Ex")
-#pragma comment(linker, "/export:D3DPERF_BeginEvent=C:\\Windows\\SysWOW64\\d3d9.D3DPERF_BeginEvent")
-#pragma comment(linker, "/export:D3DPERF_EndEvent=C:\\Windows\\SysWOW64\\d3d9.D3DPERF_EndEvent")
-#pragma comment(linker, "/export:D3DPERF_GetStatus=C:\\Windows\\SysWOW64\\d3d9.D3DPERF_GetStatus")
-#pragma comment(linker, "/export:D3DPERF_QueryRepeatFrame=C:\\Windows\\SysWOW64\\d3d9.D3DPERF_QueryRepeatFrame")
-#pragma comment(linker, "/export:D3DPERF_SetMarker=C:\\Windows\\SysWOW64\\d3d9.D3DPERF_SetMarker")
-#pragma comment(linker, "/export:D3DPERF_SetOptions=C:\\Windows\\SysWOW64\\d3d9.D3DPERF_SetOptions")
-#pragma comment(linker, "/export:D3DPERF_SetRegion=C:\\Windows\\SysWOW64\\d3d9.D3DPERF_SetRegion")
-#pragma comment(linker, "/export:DebugSetLevel=C:\\Windows\\SysWOW64\\d3d9.DebugSetLevel")
-#pragma comment(linker, "/export:DebugSetMute=C:\\Windows\\SysWOW64\\d3d9.DebugSetMute")
-#pragma comment(linker, "/export:Direct3D9EnableMaximizedWindowedModeShim=C:\\Windows\\SysWOW64\\d3d9.Direct3D9EnableMaximizedWindowedModeShim")
-#pragma comment(linker, "/export:Direct3DShaderValidatorCreate9=C:\\Windows\\SysWOW64\\d3d9.Direct3DShaderValidatorCreate9")
-#pragma comment(linker, "/export:PSGPError=C:\\Windows\\SysWOW64\\d3d9.PSGPError")
-#pragma comment(linker, "/export:PSGPSampleTexture=C:\\Windows\\SysWOW64\\d3d9.PSGPSampleTexture")
+#pragma comment(linker, "/export:_BinkClose@4=binkw32_original._BinkClose@4")
+#pragma comment(linker, "/export:_BinkDoFrame@4=binkw32_original._BinkDoFrame@4")
+#pragma comment(linker, "/export:_BinkNextFrame@4=binkw32_original._BinkNextFrame@4")
+#pragma comment(linker, "/export:_BinkOpen@8=binkw32_original._BinkOpen@8")
+#pragma comment(linker, "/export:_BinkPause@8=binkw32_original._BinkPause@8")
+#pragma comment(linker, "/export:_BinkCopyToBuffer@28=binkw32_original._BinkCopyToBuffer@28")
+#pragma comment(linker, "/export:_BinkSetPan@12=binkw32_original._BinkSetPan@12")
+#pragma comment(linker, "/export:_BinkSetVolume@12=binkw32_original._BinkSetVolume@12")
+#pragma comment(linker, "/export:_BinkGetError@0=binkw32_original._BinkGetError@0")
+#pragma comment(linker, "/export:_BinkSetIO@4=binkw32_original._BinkSetIO@4")
+#pragma comment(linker, "/export:_BinkSetSoundTrack@8=binkw32_original._BinkSetSoundTrack@8")
+#pragma comment(linker, "/export:_BinkSetSoundSystem@8=binkw32_original._BinkSetSoundSystem@8")
+#pragma comment(linker, "/export:_BinkOpenDirectSound@4=binkw32_original._BinkOpenDirectSound@4")
+#pragma comment(linker, "/export:_BinkCopyToBufferRect@44=binkw32_original._BinkCopyToBufferRect@44")
+#pragma comment(linker, "/export:_BinkWait@4=binkw32_original._BinkWait@4")
+#pragma comment(linker, "/export:_RADSetMemory@8=binkw32_original._RADSetMemory@8")
+#pragma comment(linker, "/export:_RADTimerRead@0=binkw32_original._RADTimerRead@0")
 
-typedef HRESULT(STDMETHODCALLTYPE* tEndScene)(IDirect3DDevice9*);
-typedef HRESULT(STDMETHODCALLTYPE* tCreateDevice)(IDirect3D9*, UINT, D3DDEVTYPE, HWND, DWORD, D3DPRESENT_PARAMETERS*, IDirect3DDevice9**);
+// 2. YOUR SEARCH LOGIC
+DWORD WINAPI MainThread(LPVOID lpParams) {
+    // Wait for the game to finish loading the D3D9 wrapper mod
+    Sleep(8000);
 
-tEndScene oEndScene = nullptr;
-tCreateDevice oCreateDevice = nullptr;
+    // Find the D3D9 module that the game is ACTUALLY using
+    HMODULE hD3D9 = GetModuleHandleA("d3d9.dll");
 
-HRESULT STDMETHODCALLTYPE hkCreateDevice(IDirect3D9* pD3D, UINT Adapter, D3DDEVTYPE DeviceType, HWND hFocusWindow, DWORD BehaviorFlags, D3DPRESENT_PARAMETERS* pPresentationParameters, IDirect3DDevice9** ppReturnedDeviceInterface) {
-	MessageBoxA(NULL, "", "hkCreateDevice", MB_OK);
+    if (!hD3D9) return 0;
 
-	HRESULT hr = oCreateDevice(pD3D, Adapter, DeviceType, hFocusWindow, BehaviorFlags, pPresentationParameters, ppReturnedDeviceInterface);
+    Beep(1000, 500);
 
-	if (hr == D3D_OK && ppReturnedDeviceInterface != nullptr) {
-		void** vTable = *(void***)(*ppReturnedDeviceInterface);
+    unsigned char endScenePattern[] = { 0x8B, 0xFF, 0x55, 0x8B, 0xEC, 0x6A, 0xFF };
+    uintptr_t endSceneAddr = FindPattern(hD3D9, endScenePattern, sizeof(endScenePattern));
 
-		int testIndex = 42;
+    if (!endSceneAddr) return 0;
+    DWORD old;
+    VirtualProtect((void*)endSceneAddr, 1, PAGE_EXECUTE_READWRITE, &old);
+    *(unsigned char*)endSceneAddr = 0xCC;
+    VirtualProtect((void*)endSceneAddr, 1, old, &old);
 
-		DWORD old;
-		VirtualProtect(&vTable[testIndex], sizeof(void*), PAGE_EXECUTE_READWRITE, &old);
-		vTable[testIndex] = NULL;
-		VirtualProtect(&testIndex, sizeof(void*), old, &old);
-	}
-	return hr;
-}
-
-extern "C" IDirect3D9* WINAPI Direct3DCreate9(UINT SDKVersion) {
-	MessageBoxA(NULL, "", "Direct3DCreate9", MB_OK);
-
-	HMODULE hRealD3D = LoadLibraryA("C:\\Windows\\SysWOW64\\d3d9.dll");
-	typedef IDirect3D9* (WINAPI* tD3DCreate9)(UINT);
-	tD3DCreate9 oDirect3DCreate9 = (tD3DCreate9)GetProcAddress(hRealD3D, "Direct3DCreate9");
-
-	IDirect3D9* pD3D = oDirect3DCreate9(SDKVersion);
-
-	if (pD3D) {
-		void** vTable = *(void***)pD3D;
-		DWORD old;
-		VirtualProtect(&vTable[16], sizeof(void*), PAGE_EXECUTE_READWRITE, &old);
-		oCreateDevice = (tCreateDevice)vTable[16];
-		vTable[16] = (void*)hkCreateDevice;
-		VirtualProtect(&vTable[16], sizeof(void*), old, &old);
-	}
-
-	return pD3D;
+    return 0;
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved) {
-	MessageBoxA(NULL, "", "DllMain", MB_OK);
-
-	return TRUE;
+    if (reason == DLL_PROCESS_ATTACH) {
+        CreateThread(0, 0, MainThread, 0, 0, 0);
+    }
+    return TRUE;
 }

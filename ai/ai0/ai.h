48906fe3e9dc0531b56ba9e1606b1664e0bd4e67
ele7sbghmm <elementium7@gmail.com>
2026-05-09 20:33:15 +1200

    ai

--- ai/ai0/ai.h
+++ ai/ai0/ai.h
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <wrl/client.h>
 #include <d3dx8.h>
+#pragma comment(lib, "d3dx8.lib")
 
 #include <imgui.h>
 #include <imgui_impl_win32.h>
@@ -48,9 +49,8 @@ class Ai {
 public:
     Ai() = default;
     void init() {
-        shar::d3dDisplay* display = get_shar_d3dDisplay();
-        if (!display) return;
-        m_device = display->GetD3DDevice();
+        if (shar::d3dDisplay* display = get_shar_d3dDisplay(); display != nullptr)
+            m_device = display->d3dDevice;
 
         m_device->CreateVertexBuffer(MAX_VTX * sizeof(Vtx), D3DUSAGE_WRITEONLY | D3DUSAGE_DYNAMIC, Vtx::FVF, D3DPOOL_DEFAULT, &m_vb);
 
@@ -86,6 +86,18 @@ public:
         shar::MissionStage* ms = m->mMissionStages[m->mCurrentStage];
         if (!ms) return;
 
+        for (int i = 0; i < ms->mNumConditions; ++i) {
+            auto* mc = ms->mConditions[i];
+            if (!mc) continue;
+
+            if (mc->mType == shar::MissionCondition::COND_FOLLOW_DISTANCE) {
+                auto* fc = static_cast<shar::FollowCondition*>(mc);
+                auto& p = *reinterpret_cast<D3DXVECTOR3*>(fc->mpVehicle->mTransform.m[3]);
+
+                draw_circle(data, &m_vtx_n, p, fc->mMaxDistance, 0xffff0000);
+            }
+        }
+
         if (ms->mNumVehicles <= 0) return;
         for (int i = 0; i < ms->mNumVehicles; ++i) {
             shar::MissionStage::VehicleInfo vi = ms->mVehicles[i];
@@ -94,48 +106,47 @@ public:
 
             for (int j = 0; j < vai->mNumSegments; ++j) {
                 shar::Segment s = vai->mSegments[j];
-                D3DCOLOR c = s.mType == 0 ? 0xff00ffff
-                    : s.mType == 1 ? 0xff00ff00
-                    : s.mType == 2 ? 0xff0000ff
-                    : throw;
-                data[m_vtx_n++] = { s.mStart, c };
-                data[m_vtx_n++] = { s.mEnd, c };
+                data[m_vtx_n++] = { s.mStart, 0xff00ffff };
+                data[m_vtx_n++] = { s.mEnd, 0xff0000ff };
             }
 
-            D3DXVECTOR3& d = vai->mDestination;
-            D3DXVECTOR3& nd = vai->mNextDestination;
+            if (auto* v = vai->mpVehicle; v != nullptr) {
+                D3DXVECTOR3& dest = vai->mDestination;
+                D3DXVECTOR3& nd = vai->mNextDestination;
+                auto& p = *reinterpret_cast<D3DXVECTOR3*>(v->mTransform.m[3]);
 
-            data[m_vtx_n++] = { { d.x + 2.f, d.y, d.z }, 0xffffff00 };
-            data[m_vtx_n++] = { { d.x - 2.f, d.y, d.z }, 0xffffff00 };
-            data[m_vtx_n++] = { { d.x, d.y, d.z + 2.f }, 0xffffff00 };
-            data[m_vtx_n++] = { { d.x, d.y, d.z - 2.f }, 0xffffff00 };
+                D3DXVECTOR3 delta = dest - p;
+                D3DXVec3Normalize(&delta, &delta);
 
-            data[m_vtx_n++] = { { nd.x + 2.f, nd.y, nd.z }, 0xffffffff };
-            data[m_vtx_n++] = { { nd.x - 2.f, nd.y, nd.z }, 0xffffffff };
-            data[m_vtx_n++] = { { nd.x, nd.y, nd.z + 2.f }, 0xffffffff };
-            data[m_vtx_n++] = { { nd.x, nd.y, nd.z - 2.f }, 0xffffffff };
+                D3DXVECTOR3 perp;
+                D3DXVECTOR3 up = { 0.f, 1.f, 0.f };
+                D3DXVec3Cross(&perp, &delta, &up);
+                D3DXVec3Normalize(&perp, &perp);
+
+                D3DXVECTOR3 tip_l = dest + perp - delta;
+                D3DXVECTOR3 tip_r = dest - perp - delta;
+
+                data[m_vtx_n++] = { { tip_l.x, tip_l.y, tip_l.z }, 0xff00ff00 };
+                data[m_vtx_n++] = { { dest.x, dest.y, dest.z }, 0xff00ff00 };
+                data[m_vtx_n++] = { { tip_r.x, tip_r.y, tip_r.z }, 0xff00ff00 };
+                data[m_vtx_n++] = { { dest.x, dest.y, dest.z }, 0xff00ff00 };
+                data[m_vtx_n++] = { { dest.x, dest.y, dest.z }, 0xff00ff00 };
+                data[m_vtx_n++] = { { p.x, dest.y, p.z }, 0xff00ff00 };
+            }
             
             if (vai->mType == shar::VehicleAI::AI_WAYPOINT) {
                 auto* ai = static_cast<shar::WaypointAI*>(vi.vehicleAI);
                 for (int j = 0; j < ai->miNumWayPoints; ++j) {
                     shar::WaypointAI::WayPoint wp = ai->mpWayPoints[j];
-                    D3DCOLOR c = j == ai->miCurrentWayPoint ? 0xffff0000 : 0xff00ffff;
+                    if (!wp.loc) continue;
 
-                    if (wp.lco) {
-                        draw_circle(data, &m_vtx_n, wp.lco->mLocation, ai->mTriggerRadius, c);
-                    }
-                    if (wp.seg) {
-                        D3DXVECTOR3 start = .5f * (wp.seg->mCorners[0] + wp.seg->mCorners[3]);
-                        D3DXVECTOR3 end = .5f * (wp.seg->mCorners[1] + wp.seg->mCorners[2]);
+                    D3DCOLOR c = j == ai->miCurrentWayPoint ? 0xffff0000
+                               : j == ai->miNextWayPoint ? 0xffff8000 : 0xffffff00;
 
-                        //data[m_vtx_n++] = { start, c };
-                        //data[m_vtx_n++] = { end, c };
-                    }
+                    draw_circle(data, &m_vtx_n, wp.loc->mLocation, ai->mTriggerRadius, c);
                 }
             } else if (vi.vehicleAI->mType == shar::VehicleAI::AI_CHASE) {
                 auto* ai = static_cast<shar::ChaseAI*>(vi.vehicleAI);
-            } else {
-                throw;
             }
         }
 
--- ai/ai0/shar.h
+++ ai/ai0/shar.h
@@ -16,6 +16,12 @@ namespace shar {
 
     template <typename T> struct SwapArray { int mSize, mUseSize; T* mpData; T mSwapData; };
 
+    struct SuperCam {
+        enum Type : int {
+
+        };
+    };
+
     struct Locator {
         char pad0[0x14];
         unsigned int mID;
@@ -24,11 +30,15 @@ namespace shar {
         rmt::Vector mLocation;
     };
 
+    struct Vehicle {
+        char pad0[0xb8];
+        rmt::Matrix mTransform;
+    };
 
     struct VehicleController {
         void** vfptr;
         unsigned int m_refCounted;
-        struct Vehicle* mpVehicle;
+        Vehicle* mpVehicle;
     };
 
     struct Button {
@@ -180,8 +190,9 @@ namespace shar {
     };
 
     struct WaypointAI : VehicleAI {
+        static constexpr int MAX_WAYPOINTS = 32;
         struct WayPoint {
-            Locator* lco;
+            Locator* loc;
             RoadManager::PathElement elem;
             float segT;
             RoadSegment* seg;
@@ -192,7 +203,7 @@ namespace shar {
             EVADE = 1,
             TARGET = 2
         };
-        WayPoint mpWayPoints[32];
+        WayPoint mpWayPoints[MAX_WAYPOINTS];
         int miNumWayPoints;
         int miCurrentWayPoint;
         int miNextWayPoint;
@@ -210,42 +221,165 @@ namespace shar {
         float mSecondsWaitingForPlayer;
     };
 
-    struct MissionStage {
-        enum StageTimeType {
-            STAGETIME_NOT_TIMED,
-            STAGETIME_ADD,
-            STAGETIME_SET
+    struct MissionCondition {
+        enum ConditionTypeEnum {
+            COND_INVALID,
+            COND_VEHICLE_DAMAGE,
+            COND_PLAYER_HIT,
+            COND_TIME_OUT,
+            COND_PLAYER_OUT_OF_VEHICLE,
+            COND_FOLLOW_DISTANCE,
+            COND_OUT_OF_BOUNDS,
+            COND_RACE,
+            COND_LEAVE_INTERIOR,
+            COND_POSITION,
+            COND_CARRYING_STATEPROP_COLLECTIBLE,
+            COND_NOT_ABDUCTED,
+            COND_HIT_AND_RUN_CAUGHT,
+            COND_KEEP_BARREL,
+            COND_GET_COLLECTIBLES
         };
-        struct VehicleInfo {
-            struct Vehicle* vehicle;
-            struct CarStartLocator* spawn;
-            int vehicleAINum;
-            VehicleAI* vehicleAI;
-        };
-        void* pad[0x24];//char pad0[0x84];
-        StageTimeType mStageTimeType;
-        int mStageTime;
-        int mNumVehicles;
-        VehicleInfo mVehicles[4];
-        int mNumWaypoints;
-        Locator* mWaypoints[32];
-        // ...
+        void** vfptr;
+        ConditionTypeEnum mType;
+        bool mbIsViolated;
+        bool mLeaveInteror;
+        char pad[2];
+        static bool* mFailedHitNRun;
+    };
+    bool* MissionCondition::mFailedHitNRun = reinterpret_cast<bool*>(0x0);
+
+    struct VehicleCondition : MissionCondition {
+        Vehicle* mpVehicle;
     };
 
-    struct Mission {
-        enum MissionState {
-            STATE_WAITING,
-            STATE_INPROGRESS,
-            STATE_FAILED,
-            STATE_SUCCESS
-        };
-        enum MissionStageState {
+    struct FollowCondition : VehicleCondition {
+        float mMinDistance;
+        float mMaxDistance;
+        unsigned int muTime;
+    };
+
+    struct CameraInfo {
+        SuperCam::Type type;
+        bool cut;
+        bool quickTransition;
+        bool active;
+        char pad[1];
+    };
+    struct ChaseVehicleStruct {};
+    struct SafeZone {};
+    struct LockRequirement {};
+
+    struct MissionStage {
+        static constexpr int MAX_CONDITIONS = 8;
+        enum MissionStageState : int {
             STAGE_IDLE,
             STAGE_INPROGRESS,
             STAGE_COMPLETE,
             STAGE_FAILED,
             STAGE_BACKUP,
             STAGE_ALL_COMPLETE
+        };
+        enum StageTimeType : int {
+            STAGETIME_NOT_TIMED,
+            STAGETIME_ADD,
+            STAGETIME_SET
+        };
+        enum { MAX_CHARACTERS_IN_STAGE = 6 };
+        struct VehicleInfo {
+            Vehicle* vehicle;
+            struct CarStartLocator* spawn;
+            int vehicleAINum;
+            VehicleAI* vehicleAI;
+        };
+        struct CharacterInfo {
+            char name[16];
+            struct Character* character;
+            struct CarStartLocator* locator;
+            struct CarStartLocator* carLocator;
+            struct ZoneEventLocator* pZoneEventLocator;
+            Vehicle* vehicle;
+            char VehicleName[16];
+        };
+        char pad[0x60];
+        bool mbStayBlackForStage;
+        bool mbDisablePlayerControlForCountDown;
+        char pad2[2];
+        MissionStageState mState;
+        struct MissionObjective* mObjective;
+        int mNumConditions;
+        MissionCondition* mConditions[MAX_CONDITIONS];
+        StageTimeType mStageTimeType;
+        int mStageTime;
+        int mNumVehicles;
+        VehicleInfo mVehicles[4];
+        int mNumWaypoints;
+        Locator* mWaypoints[32];
+        int mNumCharacters;
+        CharacterInfo mCharacters[MAX_CHARACTERS_IN_STAGE];
+        char mCharacterToHide[16];
+        bool mbLevelOver : 1;
+        bool mbGameOver : 1;
+        bool mbFinalStage : 1;
+        int miNameIndex;
+        int miStartMessageIndex;
+        bool mbShowMessage;
+        char pad3[3];
+        CameraInfo mCamInfo;
+        bool mMusicChange;
+        bool mKeepMusicOn;
+        char pad4[2];
+        unsigned int mMusicEventKey;
+        unsigned int mMusicStateKey;
+        unsigned int mMusicStateEventKey;
+        unsigned int mDialogEventKey;
+        std::uint64_t mConversationCharacterKey;
+        ChaseVehicleStruct m_ChaseData_Array[1];
+        SafeZone* m_SafeZone_Array[3];
+        bool mbClearTrafficForStage;
+        bool mbNoTrafficForStage;
+        bool mb_DisableHitAndRun;
+        bool mb_InsideSafeZone;
+        bool mb_UseElapsedTime;
+        char pad5[3];
+        int mRaceEntryFee;
+        bool mbPutPlayerInCar;
+        bool mbRacePaidOut;
+        bool mbSwapInDefaultCar;
+        char pad6[1];
+        char mSwapDefaultCarRespawnLocatorName[32];
+        char mSwapForceCarRespawnLocatorName[32];
+        char mSwapPlayerRespawnLocatorName[32];
+        char mPlayerRespawnLocatorName[32];
+        char mPlayerCarRespawnLocatorName[32];
+        char mTrafficDensity;
+        bool mIsBonusObjectiveStart;
+        bool mStartBonusObjectives;
+        char pad7[1];
+        LockRequirement mRequirement[2];
+        bool mMissionLocked;
+        bool mShowStageComplete : 1;
+        bool mCountdownEnabled : 1;
+        char pad8[1];
+        char mHUDIconImage[16];
+        bool mIrisAtEnd;
+        bool mFadeOutAtEnd;
+        char pad9[2];
+        float mIrisSpeed;
+        struct CountdownSequenceUnit* mCountdownSequenceUnits;
+        int mNumCountdownSequenceUnits;
+        std::uint64_t mSecondSpeakerUID;
+        bool mAllowMissionAbort : 1;
+        char pad10[3];
+        int mResetCounter;
+    };
+
+    struct Mission {
+        enum : int { MAX_BONUS_OBJECTIVES = 3 };
+        enum MissionState {
+            STATE_WAITING,
+            STATE_INPROGRESS,
+            STATE_FAILED,
+            STATE_SUCCESS
         };
         void** vfptr;
         bool mIsStreetRace1Or2;
@@ -262,8 +396,35 @@ namespace shar {
         int mMissionTimer;
         int mElapsedTimems;
         MissionState mState;
-        MissionStageState mLastStageState;
-        // ...
+        MissionStage::MissionStageState mLastStageState;
+        CarStartLocator* mVehicleRestart;
+        Locator* mPlayerRestart;
+        struct ZoneEventLocator* mDynaloadLoc;
+        ZoneEventLocator* mStreetRacePropsLoad;
+        ZoneEventLocator* mStreetRacePropsUnload;
+        int mResetToStage;
+        bool mSundayDrive : 1;
+        bool mBonusMisison : 1;
+        char pad2[3];
+        struct BonusObjective* mBonusObjectives[MAX_BONUS_OBJECTIVES];
+        unsigned int mNumBonusObjectives;
+        bool mIsForcedCar;
+        bool mbSwappedCars;
+        bool mbCarryOverOutOfCarCondition;
+        bool mbTriggerPattyAndSelmaScreen;
+        int mFinalDelay;
+        int mCompeteDelay;
+        bool mChangingStages;
+        bool mNoTimeUpdate;
+        bool mJumpBackStage;
+        char mJumpBackBy;
+        int mNumStatePropCollectibles;
+        struct StatePropCollectible** mStatePropCollectibles;
+        struct AnimatedIcon* mDoorStars;
+        int mInitPedGroupId;
+        bool mShowHUD : 1;
+        char pad3[3];
+        int mNumValidFailureHints;
     };
 
     struct GameplayManager {
@@ -279,7 +440,6 @@ namespace shar {
     };
 
     struct d3dDisplay {
-        IDirect3DDevice8* GetD3DDevice() { return d3dDevice; }
         char pad[0x120];
         IDirect3DDevice8* d3dDevice;
     };

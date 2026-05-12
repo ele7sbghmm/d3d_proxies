dc4b3ec08a0bf455a2d748774ac4e75339c9a69c
ele7sbghmm <elementium7@gmail.com>
2026-05-13 00:21:07 +1200

    .

--- sc2/proxy0/proxy.hpp
+++ sc2/proxy0/proxy.hpp
@@ -3,15 +3,26 @@
 
 inline IDirect3DDevice9* g_device;
 
-using CreateDevice_t = HRESULT(WINAPI*)(IDirect3D9*,UINT,D3DDEVTYPE,HWND,DWORD,D3DPRESENT_PARAMETERS*,IDirect3DDevice9**);
+using EndScene_t = HRESULT(WINAPI*)(IDirect3DDevice9*);
+using CreateDevice_t = HRESULT(WINAPI*)(IDirect3D9*, UINT, D3DDEVTYPE, HWND, DWORD, D3DPRESENT_PARAMETERS*, IDirect3DDevice9**);
+
+EndScene_t oEndScene = nullptr;
 CreateDevice_t oCreateDevice = nullptr;
 
-HRESULT WINAPI hCreateDevice(IDirect3D9* d3d, UINT Adapter, D3DDEVTYPE DeviceType, HWND hWnd, DWORD BehaviorFlags, D3DPRESENT_PARAMETERS* pPresentationParameters, IDirect3DDevice9** ppReturnedDeviceInterface)
+
+HRESULT WINAPI hEndScene(IDirect3DDevice9 * this_)
 {
-    HRESULT hr = oCreateDevice(d3d, Adapter, DeviceType, hWnd, BehaviorFlags, pPresentationParameters, ppReturnedDeviceInterface);
+    return oEndScene(this_);
+}
+
+HRESULT WINAPI hCreateDevice(IDirect3D9* this_, UINT Adapter, D3DDEVTYPE DeviceType, HWND hWnd, DWORD BehaviorFlags, D3DPRESENT_PARAMETERS* pPresentationParameters, IDirect3DDevice9** ppReturnedDeviceInterface)
+{
+    HRESULT hr = oCreateDevice(this_, Adapter, DeviceType, hWnd, BehaviorFlags, pPresentationParameters, ppReturnedDeviceInterface);
 
     g_device = *ppReturnedDeviceInterface;
     void** vftable = *(void***)*ppReturnedDeviceInterface;
+
+    return hr;
 }
 
 IDirect3D9* WINAPI Direct3DCreate9(UINT SDKVersion)
@@ -30,10 +41,19 @@ IDirect3D9* WINAPI Direct3DCreate9(UINT SDKVersion)
 
     IDirect3D9* d3d = oDirect3DCreate9(SDKVersion);
 
+    void** vftable = *(void***)d3d;
+
+    if (!oCreateDevice) {
+        DWORD old{};
+        VirtualProtect(&vftable[16], sizeof(void*), PAGE_EXECUTE_READWRITE, &old);
+        oCreateDevice = (CreateDevice_t)vftable[16];
+        vftable[16] = &hCreateDevice;
+        VirtualProtect(&vftable[16], sizeof(void*), old, &old);
+    }
+
     AllocConsole();
     FILE* f{};
     freopen_s(&f, "CONOUT$", "w", stdout);
-    printf("workin'");
 
     return d3d;
 }

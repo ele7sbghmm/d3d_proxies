5704d99cf84a927b7d079f59ff23772a6e6c8983
m <m@DESKTOP-IJ9O8DT>
2026-05-19 03:48:22 -0700

    .

---
+++ shar/Dll1/dllmain.cpp
@@ -0,0 +1,42 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+
+#include <d3d8.h>
+#pragma comment(linker, "/export:Direct3DCreate8:_Direct3DCreate8@4")
+
+IDirect3D8* __stdcall Direct3DCreate8(UINT SDKVersion)
+{
+    using Direct3DCreate8_t = IDirect3D8* (__stdcall*)(UINT);
+    static Direct3DCreate8_t oDirect3DCreate8 = nullptr;
+
+    if (!oDirect3DCreate8) {
+        char path[MAX_PATH];
+        GetSystemDirectoryA(path, MAX_PATH);
+        strcat_s(path, "\\d3d8.dll");
+        HMODULE hModule = LoadLibraryA(path);
+        if (!hModule) return nullptr;
+        oDirect3DCreate8 = (Direct3DCreate8_t)GetProcAddress(hModule, "Direct3DCreate8");
+    }
+
+    IDirect3D8* d3d = oDirect3DCreate8(SDKVersion);
+    if (!d3d) return nullptr;
+
+    return d3d;
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+

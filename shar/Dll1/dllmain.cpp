9a98cc4bded2cc99f2aaeabc674291e142b938fa
monte <elementium7@gmail.com>
2026-05-19 07:58:39 -0700

    .

--- shar/Dll1/dllmain.cpp
+++ shar/Dll1/dllmain.cpp
@@ -3,19 +3,20 @@
 
 #include <d3d8.h>
 
-IDirect3D8* __stdcall Direct3DCreate8(UINT SDKVersion)
-{
+#pragma comment(linker, "/export:Direct3DCreate8=_Direct3DCreate8@4")
+
+extern "C" IDirect3D8* __stdcall Direct3DCreate8(UINT SDKVersion) {
     using Direct3DCreate8_t = IDirect3D8 * (__stdcall*)(UINT);
     static Direct3DCreate8_t oDirect3DCreate8 = nullptr;
 
-    if (!oDirect3DCreate8)
-    {
+    if (!oDirect3DCreate8) {
         char path[MAX_PATH];
         GetSystemDirectoryA(path, MAX_PATH);
         strcat_s(path, "\\d3d8.dll");
         HMODULE hModule = LoadLibraryA(path);
         if (!hModule) return nullptr;
-        oDirect3DCreate8 = (Direct3DCreate8_t)GetProcAddres(hModuke, "Direct3Dcreate8");
+        oDirect3DCreate8 = (Direct3DCreate8_t)
+            GetProcAddress(hModule, "Direct3DCreate8");
     }
 
     IDirect3D8* d3d = oDirect3DCreate8(SDKVersion);
